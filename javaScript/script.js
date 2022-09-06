ea55472666bfbb44a00f8558d52a89e1b56abc18
Mariano Alvarez <marianoalvarez05@gmail.com>
2022-09-06 00:19:43 -0300

    Probando linkeos

--- javaScript/script.js
+++ javaScript/script.js
@@ -19,6 +19,7 @@ class GestionarProductos {
                     div.innerHTML = `
                     <h3>${producto.nombre}</h3>
                     <p class="precioProducto">Precio:$ ${producto.precio}</p>
+                    <img src="./imagenes/${producto.img}" alt= "Imagenes guitarras">
                     <img src="../imagenes/${producto.img}" alt= "Imagenes guitarras">
                     <p class="descProd">${producto.desc}</p>
                     <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
--- javaScript/stockParaAgCarr.js
+++ javaScript/stockParaAgCarr.js
@@ -3,22 +3,22 @@
 
 const stockProductos = [
     
-    {"id": 1, "nombre": "Stratocaster", "cantidad": 1, "precio": 1200, "img": "fender_stratocaster.jpg"},
+    {"id": 1, "nombre": "Stratocaster", "cantidad": 1, "precio": 1200},
     
-    {"id": 2, "nombre": "Les Paul", "cantidad": 1, "precio": 1500, "img": "gibson_les_paul.jpg"},
+    {"id": 2, "nombre": "Les Paul", "cantidad": 1, "precio": 1500},
     
-    {"id": 3, "nombre": "SG", "cantidad": 1, "precio": 1900, "img": "gibson_sg.jpg"},
+    {"id": 3, "nombre": "SG", "cantidad": 1, "precio": 1900},
     
-    {"id": 4, "nombre": "Telecaster", "cantidad": 1, "precio": 1800, "img": "fender_telecaster.jpg"},
+    {"id": 4, "nombre": "Telecaster", "cantidad": 1, "precio": 1800},
     
-    {"id": 5, "nombre": "Guitarras semi-acústicas", "cantidad": 1, "precio": 2500, "img": "gretsch_semiakustik.jpg"},
+    {"id": 5, "nombre": "Guitarras semi-acústicas", "cantidad": 1, "precio": 2500},
     
-    {"id": 6, "nombre": "Superstrats", "cantidad": 1, "precio": 2300, "img": "ibanez_superstrat.jpg"},
+    {"id": 6, "nombre": "Superstrats", "cantidad": 1, "precio": 2300},
     
-    {"id": 7, "nombre": "Flying V", "cantidad": 1, "precio": 3550, "img": "gibson_flying_v.jpg"},
+    {"id": 7, "nombre": "Flying V", "cantidad": 1, "precio": 3550},
     
-    {"id": 8, "nombre": "Guitarra Parker", "cantidad": 1, "precio": 3100, "img": "parker_fly.jpg"},
+    {"id": 8, "nombre": "Guitarra Parker", "cantidad": 1, "precio": 3100},
     
-    {"id": 9, "nombre": "Guitarra Harley Benton", "cantidad": 1, "precio": 2750, "img": "harley_benton.jpg"}
+    {"id": 9, "nombre": "Guitarra Harley Benton", "cantidad": 1, "precio": 2750}
     
 ]
